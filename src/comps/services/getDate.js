5bed5d71dd8348cf8c3391c4beab6df4cd619402
nrmd-naveen <nrmdnaveen@gmail.com>
2024-02-18 00:56:23 +0530

    msg aligned

--- src/comps/services/getDate.js
+++ src/comps/services/getDate.js
@@ -3,5 +3,6 @@ export const getDate = () => {
     var curr = new Date();
     curr.setDate(curr.getDate());
     var date = curr.toISOString().substring(0,10);
+    console.log("See Date : ", date)
     return date;
   }
--- src/comps/services/message.js
+++ src/comps/services/message.js
@@ -1,31 +1,21 @@
 
-// const newPerson = `***************************************
-
-// ${i+1})
-//      Name          -       ${name}
-//      Age             -       ${age}
-//      D/o              -      ${fmname}
-//      Village         -       ${village}  
-//      Phone          -      ${phone}
-// `
-
 export const message =(data,date) =>{
     console.log("msg",data)
-    const head = `Patient Details           Date : ${date}
+    const head = `*Patient Details*          *Date :* ${date}
 
-    `
+`
     let msg = head;
     data.forEach((el,i) => {
         let user = `***************************************
 
-        ${i+1})
-             Name          -       ${el.name}
-             Age             -       ${el.age}
-             D/o              -      ${el.fmname}
-             Village         -       ${el.village}  
-             Phone          -      ${el.phone}
+    ${i+1})
+        Name          -       ${el.name}
+        Age             -       ${el.age}
+        D/o              -      ${el.fmname}
+        Village         -       ${el.village}  
+        Phone          -      ${el.phone}
         
-             `
+`
         msg += user;
     });
     //console.log(msg);
