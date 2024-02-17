59e218fe2c2da7e1360f3996ad09af2d27b9ad09
nrmd-naveen <nrmdnaveen@gmail.com>
2024-02-18 02:57:01 +0530

    msg Alignment change d!

--- src/comps/services/message.js
+++ src/comps/services/message.js
@@ -1,28 +1,53 @@
 
 export const message =(data,date) =>{
+    if(data[0]===null || data===null|| data === undefined){
+        return null;
+    }
     console.log("msg",data)
     const head = `*Patient Details*          *Date :* ${date}
 
 `
+    const foot = `
+***************************************`
     let msg = head;
+    
     data.forEach((el,i) => {
+        let user = ''
         var gender = el.gender;
         if(Number(el.age)<18){
             gender = gender === 'male' ? "male child" : "female child";
         }
-        let user = `***************************************
+        if(el.street === ""|| el.street === " "||el.street === null){
+        user = `***************************************
 
-    ${i+1})
+*${i+1})*
         Name          -       ${el.name}
         Age             -       ${el.age}
-        Gender           -      ${gender} 
+        Gender        -      ${gender} 
         D/o              -      ${el.fmname}
         Village         -       ${el.village}  
         Phone          -      ${el.phone}
         
+`}  else{
+
+    user = `***************************************
+
+*${i+1})*
+        Name          -       ${el.name}
+        Age             -       ${el.age}
+        Gender        -      ${gender} 
+        D/o              -      ${el.fmname}
+        Street          -      ${el.street}
+        Village         -       ${el.village}  
+        Phone          -      ${el.phone}
+        
 `
+
+}
+
         msg += user;
     });
+    msg+=foot;
     //console.log(msg);
     return msg;
 }
