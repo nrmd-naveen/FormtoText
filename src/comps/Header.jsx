18a5f72eb55dee7ae44fa62f9bdc6879d6d8ca80
nrmd-naveen <nrmdnaveen@gmail.com>
2024-03-08 11:32:28 +0530

    Logo Path Modified

--- src/comps/Header.jsx
+++ src/comps/Header.jsx
@@ -2,7 +2,6 @@ import './../styles/Header.scss';
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import SideBar from './SideBar';
-import logo from './../assets/appLogo.png';
 
 const Header = () => {
 
@@ -32,10 +31,7 @@ const Header = () => {
                     </button> 
                 </div>  
                 <div className='hd'>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-6 h-6">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-                </svg>
-
+                <img src = './assets/appLogo.png' alt='Logo'/>
                     <h3 onClick={goToHome}>Patient Details</h3>
                 </div>
             </div>
